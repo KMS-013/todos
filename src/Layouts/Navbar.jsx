0335b87b70c02eb326cd69316e0a03b353ba1d4a
KMS-013 <kevinsavaliya013@gmail.com>
2024-05-09 16:00:00 +0530

    created home page

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Styles from "../Styles/Navbar.module.css";
+import Styles from "../Styles/Layouts/Navbar.module.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Styles from "../Styles/Pages/Home.module.css";
 
 function Home() {
   return (
-    <div>
-      <img src="/images/homeimg.svg" alt="" />
+    <div className={Styles.homecomponent}>
+      <div className={Styles.homecomponenttext}>
+        “Subtracting from your list of priorities is as important as adding to
+        it.”
+      </div>
+      <div className={Styles.homecomponentimg}>
+        <img src="/images/homeimg.svg" alt="" />
+      </div>
     </div>
   );
 }
